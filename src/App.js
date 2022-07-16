ec02e4aa4b42e11ec14dd04e846b570392629ebf
Alex Kulnis <kulnis71@gmail.com>
2022-07-16 17:25:00 +0300

    add css module

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import Card from './components/Card';
 import Header from './components/Header';
 import { Drawer } from './components/Drawer';
 
+const arr = [
+    {
+        name: 'Мужские Кроссовки Nike Air Max 270',
+        price: 12999,
+        imageUrl: '/img/1.jpg',
+    },
+    {
+        name: 'Мужские Кроссовки Rangers',
+        price: 13999,
+        imageUrl: '/img/2.jpg',
+    },
+    {
+        name: 'Мужские Кроссовки Adidas',
+        price: 14999,
+        imageUrl: '/img/3.jpg',
+    },
+    {
+        name: 'Мужские Кроссовки Nb',
+        price: 15999,
+        imageUrl: '/img/4.jpg',
+    },
+];
+
 function App() {
     return (
         <div className="wrapper clear">
@@ -19,10 +42,9 @@ function App() {
                 </div>
 
                 <div className="d-flex">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                    {arr.map((x) => (
+                        <Card key={x.name} name={x.name} price={x.price} imageUrl={x.imageUrl} />
+                    ))}
                 </div>
             </div>
         </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,21 @@
-function Card() {
+import styles from './Card.module.scss';
+
+function Card(props) {
+    const click = () => {};
+
     return (
-        <div className="card">
-            <div className="favorite">
+        <div className={styles.card}>
+            <div className={styles.favorite}>
                 <img src="/img/heart-unliked.svg" alt="Unliked" />
             </div>
-            <img width={133} height={133} src="/img/1.jpg" alt="Sneakers"></img>
-            <h5>Мужские кроссовки Nike Blazer Mid</h5>
+            <img width={133} height={133} src={props.imageUrl} alt="Sneakers"></img>
+            <h5>{props.name}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span>Цена:</span>
-                    <b>12 999 руб.</b>
+                    <b>{props.price} руб.</b>
                 </div>
-                <button className="button">
+                <button className="button" onClick={click}>
                     <img width={11} height={11} src="/img/plus.png" alt="Plus"></img>
                 </button>
             </div>
